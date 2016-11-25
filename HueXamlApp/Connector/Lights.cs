325c73bc17d58fd5d42b70be1bea15147a3a7e32
HP_FOLIO9470M\gijsb <gijs.brian.joost@hotmail.com>
2016-11-25 12:27:11 +0100

    Lights are saved

---
+++ HueXamlApp/Connector/Lights.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HueXamlApp.Connector
+{
+    public class Lights : ObservableCollection<Light>
+    {
+        public Lights(List<Light> lights)
+        {
+            foreach (var l in lights)
+            {
+                Add(l);
+            }
+        }
+
+        public void Refresh(List<Light> lights)
+        {
+            Clear();
+            foreach (var l in lights)
+            {
+                Add(l);
+            }
+        }
+    }
+}
